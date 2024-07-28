4ca37cfa25261371f13b54f5e8c838663442cc2a
MD. MARUF AHMED <modelcomputer.net@gmail.com>
2024-07-28 17:20:08 +0600

    add community page

--- js/script.js
+++ js/script.js
@@ -58,50 +58,6 @@ dropdowns.forEach((dropdown) => {
   });
 });
 
-// Events Count Down
-const second = document.querySelector("#seconds"),
-  minute = document.querySelector("#minutes"),
-  hour = document.querySelector("#hours"),
-  day = document.querySelector("#days");
-
-const countDownDate = new Date("Jul 31, 2024 00:00:00").getTime();
-
-const timeFunction = setInterval(() => {
-  // Get the current date and time
-  const now = new Date().getTime();
-
-  // Calculate the remaining time
-  const distance = countDownDate - now;
-  // console.log();
-
-  // Calculate days, hours, minutes, and seconds
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the countdown
-  day.innerHTML = days < 10 ? `0${days}` : days;
-  hour.innerHTML = hours < 10 ? `0${hours}` : hours;
-  minute.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-  second.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-
-  // If the countdown is over, display a message
-  if (distance < 0) {
-    clearInterval(timeFunction);
-
-    document.querySelector("#days").innerHTML = "00";
-    document.querySelector("#hours").innerHTML = "00";
-    document.querySelector("#minutes").innerHTML = "00";
-    document.querySelector("#seconds").innerHTML = "00";
-
-    document.querySelector(".event_close").innerHTML = "EVENT EXPIRED";
-  }
-}, 1000);
-
 // Copyright Year
 const copyrightYear = document.querySelector("#copyright_year");
 
